b98124f842282765ee97414d7e9c23c3f436f329
ayodele20011709 <lawalganiyatkofoworola0@gmail.com>
2024-10-28 00:00:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const barcode = document.querySelector(".bar-code");
+
+const navlinks = document.querySelector(".nav-links");
+
+barcode.addEventListener('click',()=>{
+    navlinks.classList.toggle("active")}
